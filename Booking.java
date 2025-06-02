3505381ac8c3587f96e1eb4154ebdf806546a499
Siratul Islam <islamsiratul@gmail.com>
2025-06-02 13:17:24 +0600

    chnages

--- Booking.java
+++ Booking.java
@@ -1,13 +1,16 @@
 public class Booking{
+
     private String username;
     private String moviename;
     private int seats;
+
     public Booking(String username,String moviename,int seats){
         this.username=username;
         this.moviename=moviename;
         this.seats=seats;
     }
+
     public void displayinfo(){
-        System.out.println("User: "+username+"|Movie:"+moviename+"|Seats"+seats);
+        System.out.println("User: " + username + "|Movie:" + moviename + "|Seats"+seats);
     }
 }
--- Person.java
+++ Person.java
@@ -1,6 +1,7 @@
-public abstract class Person implements Loginable,Displayable{
+public abstract class Person implements Loginable, Displayable{
     private String username;
     private String password;
+
     public Person(String username,String password){
         this.username=username;
         this.password=password;
@@ -10,8 +11,11 @@ public abstract class Person implements Loginable,Displayable{
         return this.username.equals(username)&&this.password.equals(password);
         
     }
+
     public abstract void showMenu();
     public String getUsername(){
-        return username;
+    
+    return username;
+    
     }
 }
